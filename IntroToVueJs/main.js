ebdca445f192c1e09daeac61c948956dbdebe10a
Juan Sebastian Buitrago <sebasbc.96@gmail.com>
2020-03-15 20:42:36 -0500

    intro to vue js - List rendering

--- IntroToVueJs/main.js
+++ IntroToVueJs/main.js
@@ -6,6 +6,18 @@ var app = new Vue({
         image : "./assets/vmSocks-green-onWhite.jpg",
         imageLink : "https://www.vuemastery.com/courses/intro-to-vue-js/attribute-binding",
         inventory : 10,
-        onSale : false
+        onSale : true,
+        details : ["80% cotton", "20% polyester", "Gender-neutral"],
+        variants : [
+            {
+                variantId : 1,
+                variantColor : "green"  
+            },
+            {
+                variantId : 2,
+                variantColor : "blue"  
+            }
+        ],
+        sizes : ["S", "M", "L"]
     }
 })
