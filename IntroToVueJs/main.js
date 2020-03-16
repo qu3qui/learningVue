19d7e35e093f0216ee6169f1bd526c3419bbce61
Juan Sebastian Buitrago <sebasbc.96@gmail.com>
2020-03-15 19:37:12 -0500

    intro to vue js -attribute binding

--- IntroToVueJs/main.js
+++ IntroToVueJs/main.js
@@ -2,6 +2,8 @@ var app = new Vue({
     el : '#app',
     data : {
         product : "Socks",
-        description : "soft and beautiful"
+        description : "soft and beautiful",
+        image : "./assets/vmSocks-green-onWhite.jpg",
+        imageLink : "https://www.vuemastery.com/courses/intro-to-vue-js/attribute-binding"
     }
 })
